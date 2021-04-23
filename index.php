aa95ae06f4fbabf44d60d38c759507f08b1f13ee
Matheus Oliveira <matheuso.silva95@gmail.com>
2021-04-23 12:53:40 -0300

    Routers

--- index.php
+++ index.php
@@ -3,16 +3,21 @@
 require __DIR__ .'/vendor/autoload.php';
 
 use \App\Http\Router;
+use \App\Http\Response;
 use \App\Controller\Pages\Home;
 
 define('URL', 'http://localhost/PHP_MVC');
 $obRouter = new Router(URL);
 
-echo '<pre>'; echo print_r($obRouter); echo '</pre>';
-exit;
+//ROTA HOME
+$obRouter->get('/',[
+    function()
+    {
+        return new Response(200,Home::getHome());
+    }
+]);
 
-echo Home::getHome();
-
-
-
+//IMPRIME O RESPONSE DA ROTA
+$obRouter->run()
+         ->sendResponse();
 ?>
