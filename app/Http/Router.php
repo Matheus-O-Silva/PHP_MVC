b1861fb82e30dd4dfeb95ba0450c6294534af51f
Matheus Oliveira <matheuso.silva95@gmail.com>
2021-04-23 08:09:44 -0300

    url e rotas

---
+++ app/Http/Router.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http;
+
+class Router
+{
+    /**
+     * URL completa do projeto (raiz)
+     * @var string
+     */
+    private $url = '';
+
+    /**
+     * Prefixo de todas as rotas
+     * @var string
+     */
+    private$prefix = '';
+
+    /**
+     * Índice de rotas
+     * @var array
+     */
+    private $routes = [];
+
+    /**
+     * Instância de Request
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * Método responsável por iniciar a classe
+     * @param string $url
+     */
+    public function __construct($url)
+    {
+        $this->request = new Request();
+        $this->url     = $url;
+    }
+}
+
+
+?>
--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 
 require __DIR__ .'/vendor/autoload.php';
 
+use \App\Http\Router;
 use \App\Controller\Pages\Home;
+
+define('URL', 'http://localhost/PHP_MVC');
+$obRouter = new Router(URL);
+
+echo '<pre>'; echo print_r($obRouter); echo '</pre>';
+exit;
 
 echo Home::getHome();
 
-//echo '<pre>'; echo print_r($teste); echo '</pre>';
+
 
 ?>
